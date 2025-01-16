9b0aa7feee73bd04183a2d6c6fc9412810098d8c
kn0pm <nisanir8@gmail.com>
2025-01-16 20:34:34 +0530

    insta1.py

--- insta1.py
+++ insta1.py
@@ -3,18 +3,21 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-service = Service("C:/Users/nisan/Downloads/Programs/edgedriver_win64/msedgedriver.exe", verbose=False)
+service = Service("C:/Users/nisan/Downloads/Programs/edgedriver_win64/msedgedriver.exe", verbose=False) #Replace my path with yours
 driver = webdriver.Edge(service=service)
 
 try:
+
+    #I used edge. So for safety copy XPath again form your browser
+    
     driver.get("https://www.instagram.com")
     time.sleep(5)  
 
     user_field = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div[1]/div[1]/div/label/input')
-    user_field.send_keys('___kn__._')
+    user_field.send_keys('username')  #Put your username here
 
     pass_field = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div[1]/div[2]/div/label/input')
-    pass_field.send_keys('$@njayKN008')
+    pass_field.send_keys('password')  #Put your password here
 
     log_but = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div[1]/div[3]')
     log_but.click()
